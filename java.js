cffa96c263578d12a68019f072cd43d8d7ef5e4e
Amarjit Paswan <137752602+Amarjit-paswan@users.noreply.github.com>
2023-07-05 20:46:31 +0530

    Update java.js

--- java.js
+++ java.js
@@ -1,4 +1,4 @@
-var end_date = "01 July 2024 12.00 AM";
+var end_date = "04 July 2024 4:18 PM";
 document.getElementById("end_time").innerText = end_date;
 
 var inputs = document.querySelectorAll("input");
